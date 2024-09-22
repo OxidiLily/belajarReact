ef3f94d3b1d812ed057d3342592955b5c9c1c6f4
OxidiLily <oxidilily@gmail.com>
2024-09-22 14:50:44 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    preview: {
+        port: 8001,
+        strictPort: true
+
+    },
+    server: {
+        host: "0.0.0.0",
+        port: 8001,
+        strictPort: true,
+        host: true,
+        origin: "http://0.0.0.0:3000"
+    }
 })
