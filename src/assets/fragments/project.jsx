658f6776b7d49d0e4812dca7d6f8be3df8c6686c
OxidiLily <oxidilily@gmail.com>
2024-10-08 09:29:45 +0700

    update

--- src/assets/fragments/project.jsx
+++ src/assets/fragments/project.jsx
@@ -13,7 +13,7 @@ function Content (){
                         </a>
                     </div>
                     <div className="p-6">
-                        <a href="https://chatbotgemini-oxidilily.streamlit.app/" target="_blank">
+                        <a href="https://github.com/OxidiLily/ChatbotGeminiTextOnly" target="_blank">
                             <h5 className="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
                             Chatbot AI
                             </h5>
@@ -23,7 +23,7 @@ function Content (){
                         </p>
                     </div>
                     <div className="p-6 pt-0">
-                        <a href="https://chatbotgemini-oxidilily.streamlit.app/" target="_blank">
+                        <a href="https://github.com/OxidiLily/ChatbotGeminiTextOnly" target="_blank">
                             <button data-ripple-light="true" type="button" className="select-none rounded-lg bg-blue-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none hover:animate-bounce">
                             Go
                             </button>
