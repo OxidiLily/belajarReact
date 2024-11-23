81791d4cd55b6850677226ef0f76f0c35f69e536
OxidiLily <oxidilily@gmail.com>
2024-11-24 01:37:49 +0700

    fix bug responsive

--- src/assets/fragments/footer.jsx
+++ src/assets/fragments/footer.jsx
@@ -24,14 +24,14 @@ function Footer(){
     }
     return(
         <>
-            <div className="text-white p-4 flex justify-between items-center mt-20">
+            <div className="text-white p-4 flex-row justify-between items-center mt-20">
                 <div className="ml-1 flex ">
                     <div className="grid grid-flow-col auto-cols-max">
                         <img src={logo} alt="Logo" className="w-14 pt-9" />
                         <p className="pt-12 ml-1 font-bold">Tekad Agung Nugroho</p>
                     </div>
                 </div>
-                <div className="mr-1 text-white">
+                <div className="flex mx-auto text-white">
                     <button onClick={Sosmed} id="btnSosmed" className="group sm:hidden relative inline-block w-[44px] p-[5px] h-[45px] m-[25px] pt-5">
                         <span
                             className="mx-[auto] my-[0] relative top-[12px] w-[30px] h-[6px] bg-white block [transition-property:margin,_width] group-hover:w-[20px] duration-200 after:absolute after:content-[''] after:mt-[12px] after:w-[30px] after:h-[6px] after:bg-white after:block after:left-[0] after:[transition-property:margin,_left] after:duration-200 group-hover:after:mt-[6px] group-hover:after:-left-[5px] before:absolute before:content-[''] before:-mt-[12px] before:w-[30px] before:h-[6px] before:bg-white before:block before:left-[0] before:[transition-property:margin,_width,_left] before:duration-200 group-hover:before:-mt-[6px] group-hover:before:w-[10px] group-hover:before:left-[5px]"
