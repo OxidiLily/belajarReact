5ef976582cf062e0ae75560765db8cab67211fa6
Tekad Agung Nugroho <tekadpcx@gmail.com>
2025-02-05 08:10:01 +0700

    update link

--- src/assets/fragments/project2.jsx
+++ src/assets/fragments/project2.jsx
@@ -102,7 +102,7 @@ function Content2 (){
                         </p>
                     </div>
                     <div className="p-6 pt-0">
-                        <a href="https://github.com/OxidiLily/streamlit-ollama-local.git" target="_blank">
+                        <a href="https://ai-webui.oxidilily.my.id/" target="_blank">
                             <button data-ripple-light="true" type="button" className="select-none rounded-lg bg-blue-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none hover:animate-bounce">
                             Go
                             </button>
