9b99ca0ed2ddd204febccf3e1f5119a12fbf64ee
Tekad Agung Nugroho <tekadpcx@gmail.com>
2025-02-05 07:38:29 +0700

    update web

--- src/assets/fragments/project2.jsx
+++ src/assets/fragments/project2.jsx
@@ -1,6 +1,7 @@
 import BotMinecraft from "../../images/botAfk.png"
 import ar from "../../images/ar.png"
 import botDiscord from "../../images/discord.png"
+import Ollama from "../../images/ollama.png"
 function Content2 (){
     return (
         <>
@@ -76,6 +77,32 @@ function Content2 (){
                     </div>
                     <div className="p-6 pt-0">
                         <a href="https://github.com/OxidiLily/BotMinecraft-docker" target="_blank">
+                            <button data-ripple-light="true" type="button" className="select-none rounded-lg bg-blue-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none hover:animate-bounce">
+                            Go
+                            </button>
+                        </a>
+                    </div>
+                </div>
+            </div>  
+            <div className="lg:flex mt-8 sm:mt-10 md:mt-10 lg:mt-0 mx-auto">
+                <div className="relative flex w-80 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+                    <div className="relative mx-4 -mt-6 h-40 overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40 bg-gradient-to-r from-blue-500 to-blue-600">
+                    <a href="https://github.com/OxidiLily/streamlit-ollama-local.git" target="_blank">
+                        <img src={Ollama} alt="Streamlit" className="relative object-fit h-44" />
+                    </a>
+                    </div>
+                    <div className="p-6">
+                        <a href="https://github.com/OxidiLily/streamlit-ollama-local.git" target="_blank">
+                            <h5 className="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+                            Buat Streamlit AI Local
+                            </h5>
+                        </a>                    
+                        <p className="block font-sans text-base font-normal italic leading-relaxed text-inherit antialiased">
+                        AI Model, docker dan streamlit.
+                        </p>
+                    </div>
+                    <div className="p-6 pt-0">
+                        <a href="https://github.com/OxidiLily/streamlit-ollama-local.git" target="_blank">
                             <button data-ripple-light="true" type="button" className="select-none rounded-lg bg-blue-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none hover:animate-bounce">
                             Go
                             </button>
